556e3bada825793de1dce316dfa12e763acdc705
Backendeng <backendeng0417@gmail.com>
2022-10-03 14:14:51 -0700

    import package
    Texas_HoldEm_Poker

---
+++ Assets/TexasHoldEmProject/Script/BBSoundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BLabTexasHoldEmProject {
+
+public class BBSoundController : MonoBehaviour {
+
+ public AudioClip pickCard;
+ public AudioClip moveCard;
+ public AudioClip chipHandle;
+ public AudioClip cardsShuffle;
+ public AudioClip genBip;
+ public AudioClip rotateCard;
+ [Header("Only Multiplayer")]
+ public AudioClip cleanCards;
+ [Header("Only Multiplayer")]
+ public AudioClip cleanChips;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playClip(AudioClip ac) {
+	  GetComponent<AudioSource>().PlayOneShot(ac);
+	}
+
+}
+}
