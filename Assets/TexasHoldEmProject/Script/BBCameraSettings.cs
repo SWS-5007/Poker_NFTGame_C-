387e52550e7df7149becf8d3b858f0edc170085d
Backendeng <backendeng0417@gmail.com>
2022-10-09 09:30:06 -0700

    Code Beauty

--- Assets/TexasHoldEmProject/Script/BBCameraSettings.cs
+++ Assets/TexasHoldEmProject/Script/BBCameraSettings.cs
@@ -1,34 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace BLabTexasHoldEmProject {
+namespace BLabTexasHoldEmProject
+{
+    public class BBCameraSettings : MonoBehaviour
+    {
+        public float setting_16_9 = 3.53f;
+        public float setting_3_2 = 3.99f;
+        public float setting_4_3 = 4.49f;
 
-public class BBCameraSettings : MonoBehaviour {
+        void Start()
+        {
+            string ratio = BBStaticData.getRatio(GetComponent<Camera>());
 
-	public float setting_16_9 = 3.53f;
-	public float setting_3_2 = 3.99f;
-	public float setting_4_3 = 4.49f;
+            Debug.Log("----------------------------->> Ratio : " + ratio);
 
-	void Start () {
-
-		string ratio = BBStaticData.getRatio(GetComponent<Camera>());
-
-		Debug.Log("----------------------------->> Ratio : " + ratio);
-
-		switch(ratio) {
-		case "16:9":
-		 GetComponent<Camera>().orthographicSize = setting_16_9;
-		break;
-		case "3:2":
-			GetComponent<Camera>().orthographicSize = setting_3_2;
-		break;
-		case "4:3":
-			GetComponent<Camera>().orthographicSize = setting_4_3;
-		break;
-		}
-
-	}
-
-		
-}
+            switch (ratio)
+            {
+                case "16:9":
+                    GetComponent<Camera>().orthographicSize = setting_16_9;
+                    break;
+                case "3:2":
+                    GetComponent<Camera>().orthographicSize = setting_3_2;
+                    break;
+                case "4:3":
+                    GetComponent<Camera>().orthographicSize = setting_4_3;
+                    break;
+            }
+        }
+    }
 }
--- Assets/TexasHoldEmProject/Script/BBMoneyController.cs
+++ Assets/TexasHoldEmProject/Script/BBMoneyController.cs
@@ -2,36 +2,50 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-namespace BLabTexasHoldEmProject {
+namespace BLabTexasHoldEmProject
+{
+    public class BBMoneyController : MonoBehaviour
+    {
+        BBGameController _BBGameController;
 
-public class BBMoneyController : MonoBehaviour {
+        // Use this for initialization
+        void Awake()
+        {
+            _BBGameController = GetComponent<BBGameController>();
 
-
-	BBGameController _BBGameController; 
-
-	// Use this for initialization
-	void Awake () {
-
-				_BBGameController = GetComponent<BBGameController>();
-
-				  if(_BBGameController._BBGlobalDefinitions.isAnOpenGame) {
-				     for(int x = 0; x < _BBGameController.playerDataList.Count;x++) {
-				       _BBGameController.playerDataList[x].currentPlayerTotalMoney = _BBGameController._BBGlobalDefinitions.playersCashDuringOpenGame[x];
-				       _BBGameController.playerDataList[x].runOutOfMoney = _BBGameController._BBGlobalDefinitions.playersStateIsOutDuringOpenGame[x];
-				     }
-							_BBGameController._BBGlobalDefinitions.smallBlindValue = _BBGameController._BBGlobalDefinitions.limitedLow * BBStaticData.cardsHandProgressive;
-							_BBGameController._BBGlobalDefinitions.bigBlindValue = _BBGameController._BBGlobalDefinitions.limitedHight * BBStaticData.cardsHandProgressive;
-				  } else {
-				     _BBGameController._BBGlobalDefinitions.resetInitialPlayerIsOutState();
-				     _BBGameController._BBGlobalDefinitions.loadInitialPlayersCashDuringGame();
-					 _BBGameController._BBGlobalDefinitions.isAnOpenGame = true;
-					 if(_BBGameController._BBGlobalDefinitions.useSimulateMoneyValueForLocalPlayer) {
-							_BBGameController._BBGlobalDefinitions.playersCashDuringOpenGame[_BBGameController._BBGlobalDefinitions.localPlayer] = _BBGameController._BBGlobalDefinitions.localPlayerMoneyValueSimulation;
-					 }
-						_BBGameController._BBGlobalDefinitions.smallBlindValue = _BBGameController._BBGlobalDefinitions.limitedLow;
-						_BBGameController._BBGlobalDefinitions.bigBlindValue = _BBGameController._BBGlobalDefinitions.limitedHight;
-				  }
-				  /*
+            if (_BBGameController._BBGlobalDefinitions.isAnOpenGame)
+            {
+                for (int x = 0; x < _BBGameController.playerDataList.Count; x++)
+                {
+                    _BBGameController.playerDataList[x].currentPlayerTotalMoney =
+                        _BBGameController._BBGlobalDefinitions.playersCashDuringOpenGame[x];
+                    _BBGameController.playerDataList[x].runOutOfMoney =
+                        _BBGameController._BBGlobalDefinitions.playersStateIsOutDuringOpenGame[x];
+                }
+                _BBGameController._BBGlobalDefinitions.smallBlindValue =
+                    _BBGameController._BBGlobalDefinitions.limitedLow
+                    * BBStaticData.cardsHandProgressive;
+                _BBGameController._BBGlobalDefinitions.bigBlindValue =
+                    _BBGameController._BBGlobalDefinitions.limitedHight
+                    * BBStaticData.cardsHandProgressive;
+            }
+            else
+            {
+                _BBGameController._BBGlobalDefinitions.resetInitialPlayerIsOutState();
+                _BBGameController._BBGlobalDefinitions.loadInitialPlayersCashDuringGame();
+                _BBGameController._BBGlobalDefinitions.isAnOpenGame = true;
+                if (_BBGameController._BBGlobalDefinitions.useSimulateMoneyValueForLocalPlayer)
+                {
+                    _BBGameController._BBGlobalDefinitions.playersCashDuringOpenGame[
+                        _BBGameController._BBGlobalDefinitions.localPlayer
+                    ] = _BBGameController._BBGlobalDefinitions.localPlayerMoneyValueSimulation;
+                }
+                _BBGameController._BBGlobalDefinitions.smallBlindValue =
+                    _BBGameController._BBGlobalDefinitions.limitedLow;
+                _BBGameController._BBGlobalDefinitions.bigBlindValue =
+                    _BBGameController._BBGlobalDefinitions.limitedHight;
+            }
+            /*
 							Debug.Log("[BBMoneyController] =============================== START =================================================>> isOpenGame : " + 
 							_BBGameController._BBGlobalDefinitions.isAnOpenGame +
 							" smallBlindValue : " + _BBGameController._BBGlobalDefinitions.smallBlindValue +
@@ -39,79 +53,98 @@ public class BBMoneyController : MonoBehaviour {
 							" cardsHandProgressive : " + BBStaticData.cardsHandProgressive
 							);
 				  */
-	}
+        }
 
-	public bool checkForBetPossibility (int playerID, float valueToBet) {
-
-/*		Debug.Log("[BBMoneyController] =============================== checkForBetPossibility =================================================>> player : " 
+        public bool checkForBetPossibility(int playerID, float valueToBet)
+        {
+            /*		Debug.Log("[BBMoneyController] =============================== checkForBetPossibility =================================================>> player : " 
 		+ playerID 
 		+  " value : " + valueToBet + 
 			" playerMoney : " + _BBGameController.playerDataList[playerID].currentPlayerTotalMoney
 		);
 */
 
-		 if(SceneManager.GetActiveScene().name.Contains("Multiplayer")) {
-		 } else {
-				if(_BBGameController.allInInAction) {
-					return true;
-				} else {
-					if(_BBGameController.playerDataList[playerID].currentPlayerTotalMoney >= valueToBet) {
-					  return true;
-					}
-				}
-		}
-       return false;
-	}
+            if (SceneManager.GetActiveScene().name.Contains("Multiplayer")) { }
+            else
+            {
+                if (_BBGameController.allInInAction)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (
+                        _BBGameController.playerDataList[playerID].currentPlayerTotalMoney
+                        >= valueToBet
+                    )
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
-	public float getBetValue (BBGlobalDefinitions.GamePhaseDetail gamePhase) {
+        public float getBetValue(BBGlobalDefinitions.GamePhaseDetail gamePhase)
+        {
+            float raiseBetVal = 0;
 
-		float raiseBetVal = 0;
+            switch (gamePhase)
+            {
+                case BBGlobalDefinitions.GamePhaseDetail.FirstBettingRound:
 
-	   switch(gamePhase) {
-	      case BBGlobalDefinitions.GamePhaseDetail.FirstBettingRound: 
-			       
-			       switch(_BBGameController._BBGlobalDefinitions.roundRaiseCounter) {
-				     case 1: raiseBetVal = _BBGameController._BBGlobalDefinitions.smallBlindValue * 2;
-				      break; 
-			         case 2: raiseBetVal = _BBGameController._BBGlobalDefinitions.smallBlindValue * 3;
-				     break; 
-			         case 3: raiseBetVal = _BBGameController._BBGlobalDefinitions.smallBlindValue * 4;
-				     break;
-				     default:
-				       raiseBetVal = _BBGameController._BBGlobalDefinitions.smallBlindValue;
-				     break;
-				    }
+                    switch (_BBGameController._BBGlobalDefinitions.roundRaiseCounter)
+                    {
+                        case 1:
+                            raiseBetVal =
+                                _BBGameController._BBGlobalDefinitions.smallBlindValue * 2;
+                            break;
+                        case 2:
+                            raiseBetVal =
+                                _BBGameController._BBGlobalDefinitions.smallBlindValue * 3;
+                            break;
+                        case 3:
+                            raiseBetVal =
+                                _BBGameController._BBGlobalDefinitions.smallBlindValue * 4;
+                            break;
+                        default:
+                            raiseBetVal = _BBGameController._BBGlobalDefinitions.smallBlindValue;
+                            break;
+                    }
+                    break;
+            }
 
-	      break;
-	   }
+            return raiseBetVal;
+        }
 
+        public void setPlayerOutForNoMoreMoney(int playerID, float valueBeingBet)
+        {
+            if (SceneManager.GetActiveScene().name.Contains("Multiplayer")) { }
+            else
+            {
+                if (_BBGameController.allInInAction) { }
+                else
+                {
+                    GetComponent<BBGuiInterface>().setPlayerOUTGame(playerID);
+                    _BBGameController.playerDataList[playerID].isOutOfGame = true;
 
-	   return raiseBetVal;
+                    _BBGameController.playerDataList[playerID].currentPlayerTotalMoney +=
+                        valueBeingBet;
 
-	}
-
-	public void setPlayerOutForNoMoreMoney(int playerID, float valueBeingBet) {
-
-			if(SceneManager.GetActiveScene().name.Contains("Multiplayer")) {
-			} else {
-				 if(_BBGameController.allInInAction) {
-
-				 } else {
-					GetComponent<BBGuiInterface>().setPlayerOUTGame(playerID);
-					_BBGameController.playerDataList[playerID].isOutOfGame = true;
-
-					_BBGameController.playerDataList[playerID].currentPlayerTotalMoney += valueBeingBet;
-
-					if(playerID == _BBGameController._BBGlobalDefinitions.localPlayer) {
-						_BBGameController.UIMoveingController.SetActive(false);
-						_BBGameController.PanelLocalPlayerLose.SetActive(true);
-					} else {
-						_BBGameController._BBGlobalDefinitions.playersStateIsOutDuringOpenGame[playerID] = true;
-						_BBGameController.playerDataList[playerID].runOutOfMoney = true;
-					}
-			   }
-		   }
-	}
-
-}
+                    if (playerID == _BBGameController._BBGlobalDefinitions.localPlayer)
+                    {
+                        _BBGameController.UIMoveingController.SetActive(false);
+                        _BBGameController.PanelLocalPlayerLose.SetActive(true);
+                    }
+                    else
+                    {
+                        _BBGameController._BBGlobalDefinitions.playersStateIsOutDuringOpenGame[
+                            playerID
+                        ] = true;
+                        _BBGameController.playerDataList[playerID].runOutOfMoney = true;
+                    }
+                }
+            }
+        }
+    }
 }
